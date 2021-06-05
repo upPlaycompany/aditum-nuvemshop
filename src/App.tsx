e968fae030a4dc6c97872b0470a8ad888ece2988
upPlaycompany <upplay.company@gmail.com>
2021-06-04 23:59:19 -0400

    Adição de estilizações

--- src/App.tsx
+++ src/App.tsx
@@ -4,49 +4,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <main >
-        <div className="text-left">
-          <h4>Aditum pagamentos</h4>
-          <h6>Método de pagamentos aditum</h6>
-        </div>
-        <div className="">
-          <Form>
-            <Form.Group>
-              <Form.Control size="sm" as="select">
-                <option>Yes</option>
-                <option>No</option>
-              </Form.Control>
-              <Form.Control size="sm" as="select">
-                <option>Yes</option>
-                <option>No</option>
-              </Form.Control>
-              <Form.Control size="sm" as="select">
-                <option>Yes</option>
-                <option>No</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>ID do antiffraude</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-    </Form.Text>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>CNPJ</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-  </Button></Form>
-        </div>
-      </main>
-    </div>
+    <details>
+      <summary className="text-large">Aditum pagamentos</summary>
+      <div className="App">
+        <main >
+          <div className="text-left">
+            <h6>Método de pagamentos aditum</h6>
+          </div>
+          <div className="">
+            <Form>
+              <Form.Group>
+                <Form.Control size="sm" as="select">
+                  <option>Yes</option>
+                  <option>No</option>
+                </Form.Control>
+                <Form.Control size="sm" as="select">
+                  <option>Yes</option>
+                  <option>No</option>
+                </Form.Control>
+                <Form.Control size="sm" as="select">
+                  <option>Yes</option>
+                  <option>No</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>ID do antiffraude</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+          </Form.Text>
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>CNPJ</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+        </Button>
+            </Form>
+          </div>
+        </main>
+      </div>
+    </details>
   );
 }
 
